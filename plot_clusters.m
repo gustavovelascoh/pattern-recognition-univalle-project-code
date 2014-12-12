fb0a5514adbc084867367288c5ea322fa0ec55fd
Gustavo Velasco Hernández <gustavo.velascoh@gmail.com>
2014-12-12 01:03:59 -0500

    Fixed bug in plot_clusters function

--- plot_clusters.m
+++ plot_clusters.m
@@ -1,10 +1,10 @@
 function plot_clusters(clusters, idx, n_plots)
 
-c_styles = {'.b', '.r', '.g', '.m', '.k', '.c','.b', '.r', '.g', '.m', '.k', '.c','.b', '.r', '.g'};
+c_styles = {'.b', '.r', '.g', '.m', '.k', '.c', '.y', '.b', '.r', '.g', '.m', '.k', '.c','.b', '.r'};
 
 if n_plots == 3
 for i=1:3
-		n_clus = length(clusters{i})
+		n_clus = length(clusters{idx+i-1})
 		subplot(1,3,i);
 		for j=1:n_clus
 			plot(clusters{idx+i-1}{j}.pts(:,1),clusters{idx+i-1}{j}.pts(:,2),c_styles{j}); hold on; 
@@ -14,7 +14,7 @@ end
 
 if n_plots == 6
 for i=1:6
-		n_clus = length(clusters{i})
+		n_clus = length(clusters{idx+i-1})
 		subplot(2,3,i);
 		for j=1:n_clus
 			plot(clusters{idx+i-1}{j}.pts(:,1),clusters{idx+i-1}{j}.pts(:,2),c_styles{j}); hold on; 
