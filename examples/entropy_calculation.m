ea7170aa47d845ecdee8b885b37becee7b947bf1
Gustavo Velasco-Hernandez <gustavo.velascoh@gmail.com>
2015-06-12 11:57:01 -0500

    Refined entropy_calculation script

--- examples/entropy_calculation.m
+++ examples/entropy_calculation.m
@@ -1,11 +1,18 @@
-l = size(laser_data, 2);
+l = size(ld, 2);
 
 e0 = zeros(l,1);
-e1 = zeros(l,1);
-e2 = zeros(l,1);
+%e1 = zeros(l,1);
+%e2 = zeros(l,1);
+%e3 = zeros(l,1);
 
 for i=1:l
-	e0(i) = entropy(laser_data(:,i));
-	e1(i) = get_entropy(laser_data(:,i));
-	e2(i) = entropy2(laser_data(:,i));
+	e0(i) = entropy(ld(:,i));
+	%e1(i) = get_entropy(ld(:,i));
+	%e2(i) = entropy2(ld(:,i));
+
+	%[n, h] = hist(ld(:,i), 256);
+	%nn = n/sum(n);
+	%e3(i) = -sum(nn(nn~=0).*log10(nn(nn~=0)));
 end
+
+plot(e0), xlabel('Frame'), ylabel('Entropy')
