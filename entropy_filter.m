b4cf213cb580b8da1b4ced3df9cfb3c7a4f1e330
Gustavo Velasco-Hernandez <gustavo.velascoh@gmail.com>
2015-08-02 21:21:08 -0500

    Improved entropy filter for allowing input parameters and default values

--- entropy_filter.m
+++ entropy_filter.m
@@ -1,21 +1,17 @@
-function [ef, v, u, e_th] = entropy_filter(ef, e)
-
-BUF_SIZE = 64;
+function [ef, v, u, e_th] = entropy_filter(ef, e, BUF_SIZE = 64, K=6)
 
 curr_len = length(ef);
 
 u = mean(ef);
 
-e_th = u-6*std(ef);
+e_th = u-K*std(ef);
 v=0;
 
 if (e >= e_th)
-	if curr_len >= 8
+	if curr_len >= BUF_SIZE
 		ef = [ef(2:end) e];
 	else		
 		ef = [ef e];		
-	end
-	
+	end	
 	v = 1;
 end
-
