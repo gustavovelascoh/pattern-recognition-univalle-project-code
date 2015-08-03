0a475a31b017fe55f96decbd6a95cb8ec0cea898
Gustavo Velasco-Hernandez <gustavo.velascoh@gmail.com>
2015-08-02 21:23:15 -0500

    Added time execution measuring for entropy filtering script

--- examples/entropy_filtering.m
+++ examples/entropy_filtering.m
@@ -4,13 +4,16 @@ le = length(e0);
 
 u = zeros(1, le);
 e_th = zeros(1, le);
+t_ex0 = zeros(1, le);
 
 ef = 0;
 df = [];
 
 for i=1:le
 
+	tic;
 	[ef, v, u(i), e_th(i)] =  entropy_filter(ef, e0(i));
+	t_ex0(i) = toc;
 
 	if (~v)
 		df = [df i];
