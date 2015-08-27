6cb38a4f1df7353eadcf9b0f17af40791d12cb57
Gustavo Velasco-Hernandez <gustavo.velascoh@gmail.com>
2015-08-26 23:19:46 -0500

    Added time-triggered-based capture

--- test_filtering.m
+++ test_filtering.m
@@ -21,38 +21,30 @@
 % AVG: mean(ACC + TPR + TNR + FPR)
 %
 
-
-bss = [64];
+bss = [8, 16, 32, 64];
 ks = [1,2,3,4, 5, 6, 7, 8, 9, 10,11,12];
 tl = length(bss)*length(ks);
 tn = 1;
 
 test_out=zeros(tl, 12);
 
-
 for bj=bss
 	for kj=ks
 		le = length(e0);
-
-		u = zeros(1, le);
-		e_th = zeros(1, le);
-		t_ex0 = zeros(1, le);
 
 		ef = 0;
 		df = [];
 
 		for i=1:le
 
-			tic;
-			[ef, v, u(i), e_th(i)] =  entropy_filter(ef, e0(i), bj, kj);
-			t_ex0(i) = toc;
+			[ef, v] =  entropy_filter(ef, e0(i), bj, kj);
 
 			if (~v)
 				df = [df i];
 			end
 		end
 
-		[c, m] = filter_performance(lms1_df_gt, df, e0);
+		[c, m] = filter_performance(df_gt, df, e0);
 
 		test_out(tn, :) = [bj, kj, c, m, mean(m(1:4))];
 		tn++;
